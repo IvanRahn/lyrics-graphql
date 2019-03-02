5b4155a715986413efffdf9b4e6b594b06f30c3a
Ivan <ivan.rahn@gmail.com>
2019-03-02 14:08:14 +1100

    add query

--- client/index.js
+++ client/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from "apollo-client";
 import {ApolloProvider} from "react-apollo";
+import {Router, Route, hashHistory, IndexRoute} from "react-router";
+import App from "./components/App"
 import SongList from "./components/SongList"
+import SongCreate from './components/SongCreate';
 const Root = () => {
 const client = new ApolloClient({})
   return (
   <ApolloProvider client={client}>
-    <SongList/>
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+    <IndexRoute component={SongList}/>
+    </Route>
+    <Route path="/song/new" component={SongCreate}/>
+    </Router>
   </ApolloProvider>
   )
 };
